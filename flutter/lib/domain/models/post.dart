fd126270f10dd6bb14bb8f1ed6ad4aaa870570a3
sesta <sesta.ec5@gmail.com>
2020-06-14 19:07:10 +0900

    画像をリサイズして保存できるようにする

--- flutter/lib/domain/models/post.dart
+++ flutter/lib/domain/models/post.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:bacchus/repository/provider/firestore.dart';
@@ -5,12 +6,19 @@ import 'package:bacchus/repository/provider/storage.dart';
 import 'package:multi_image_picker/multi_image_picker.dart';
 
 const String BASE_IMAGE_PATH =  'post_images';
+// スマホに表示する場合に安心できそうな横幅サイズ
+const int ORIGINAL_WIDTH_SIZE = 1280;
+const int THUMB_WIDTH_SIZE = 640;
 
 void post(List<Asset> images) async {
   // TODO: 複数画像の対応する
   // TODO: 画像の容量をどうにかする
   // TODO: 画像の内容をチェックする
-  ByteData byteData = await images[0].getByteData();
+  double resizeRate = min(THUMB_WIDTH_SIZE / images.first.originalWidth, 1);
+  ByteData byteData = await images[0].getThumbByteData(
+    (images.first.originalWidth * resizeRate).round(),
+    (images.first.originalHeight * resizeRate).round(),
+  );
   List<int> imageData = byteData.buffer.asUint8List();
   int timestamp = DateTime.now().millisecondsSinceEpoch;
   final String imageName = 'image_$timestamp';
@@ -32,3 +40,4 @@ void post(List<Asset> images) async {
     'imagePath': '$BASE_IMAGE_PATH/$imageName',
   });
 }
+
