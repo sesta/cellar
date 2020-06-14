76e8a0a5f34d4ca56b1618425795731358c7a703
sesta <sesta.ec5@gmail.com>
2020-06-14 20:46:24 +0900

    サムネと複数画像をアップロードできるようにする

--- flutter/lib/domain/models/post.dart
+++ flutter/lib/domain/models/post.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 import 'dart:typed_data';
 
@@ -8,20 +9,38 @@ import 'package:multi_image_picker/multi_image_picker.dart';
 const String BASE_IMAGE_PATH =  'post_images';
 // スマホに表示する場合に安心できそうな横幅サイズ
 const int ORIGINAL_WIDTH_SIZE = 1280;
+// 2カラムを想定したサイズ
 const int THUMB_WIDTH_SIZE = 640;
 
 void post(List<Asset> images) async {
-  // TODO: 複数画像の対応する
-  // TODO: 画像の容量をどうにかする
+  final int timestamp = DateTime.now().millisecondsSinceEpoch;
+
   // TODO: 画像の内容をチェックする
-  double resizeRate = min(THUMB_WIDTH_SIZE / images.first.originalWidth, 1);
-  ByteData byteData = await images[0].getThumbByteData(
-    (images.first.originalWidth * resizeRate).round(),
-    (images.first.originalHeight * resizeRate).round(),
+  // TODO: 識別子にタイムスタンプ以外も追加する
+  final String thumbImageName = 'image-$timestamp/thumb';
+  await uploadImage(images.first, thumbImageName, THUMB_WIDTH_SIZE);
+
+  final List<String> imagePaths = [];
+  for (int index = 0 ; index < images.length ; index++) {
+    final String originalImageName = 'image-$timestamp/original-$index';
+    await uploadImage(images[index], originalImageName, ORIGINAL_WIDTH_SIZE);
+    imagePaths.add('$BASE_IMAGE_PATH/$originalImageName');
+  }
+
+  addDate('posts', {
+    'imagePaths': imagePaths,
+    'timestamp': timestamp,
+    'thumbImagePath': '$BASE_IMAGE_PATH/$thumbImageName',
+  });
+}
+
+Future<void> uploadImage(Asset image, String imageName, int expectWidthSize) async {
+  double resizeRate = min(expectWidthSize / image.originalWidth, 1);
+  ByteData byteData = await image.getThumbByteData(
+    (image.originalWidth * resizeRate).round(),
+    (image.originalHeight * resizeRate).round(),
   );
   List<int> imageData = byteData.buffer.asUint8List();
-  int timestamp = DateTime.now().millisecondsSinceEpoch;
-  final String imageName = 'image_$timestamp';
   final int error = await uploadData(
     BASE_IMAGE_PATH,
     imageName,
@@ -30,14 +49,9 @@ void post(List<Asset> images) async {
   );
 
   if (error != null) {
-    print('error: $error');
-    return ;
+    throw new Exception('アップロードに失敗しました: error: $error');
   }
 
-  print('upload success');
-  addDate('posts', {
-    'timestamp': timestamp,
-    'imagePath': '$BASE_IMAGE_PATH/$imageName',
-  });
+  print('Upload Success: $imageName');
 }
 
--- flutter/lib/domain/models/timeline.dart
+++ flutter/lib/domain/models/timeline.dart
@@ -7,7 +7,7 @@ Future<List<String>> getTimelineImageUrls() async {
   final List<String> imageUrls = [];
   final posts = await getAll('posts');
   await Future.forEach(posts, (post) async {
-    final imageUrl = await getDataUrl(post['imagePath']);
+    final imageUrl = await getDataUrl(post['thumbImagePath']);
     imageUrls.add(imageUrl);
   });
 
