c089fcaa12f40fc23dfbca5a2454db2baff1a6ee
sesta <sesta.ec5@gmail.com>
2020-06-14 15:57:43 +0900

    無駄なprintを削除

--- flutter/lib/domain/models/timeline.dart
+++ flutter/lib/domain/models/timeline.dart
@@ -7,9 +7,7 @@ Future<List<String>> getTimelineImageUrls() async {
   final List<String> imageUrls = [];
   final posts = await getAll('posts');
   await Future.forEach(posts, (post) async {
-    final path = post['imagePath'];
-    print(path);
-    final imageUrl = await getDataUrl(path);
+    final imageUrl = await getDataUrl(post['imagePath']);
     imageUrls.add(imageUrl);
   });
 
