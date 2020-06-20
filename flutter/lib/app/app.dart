7fd5c2bfb942e43a504a4b145d95eb1ca8e39bb8
sesta <sesta.ec5@gmail.com>
2020-06-20 13:28:47 +0900

    GoogleAuthが動くようにする

--- flutter/lib/app/app.dart
+++ flutter/lib/app/app.dart
@@ -1,14 +1,44 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_analytics/observer.dart';
 
 import 'package:bacchus/app/pages/home.dart';
 
 class Bacchus extends StatelessWidget {
   static FirebaseAnalytics analytics = FirebaseAnalytics();
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<FirebaseUser> _handleSignIn() async {
+    GoogleSignInAccount googleCurrentUser = _googleSignIn.currentUser;
+    try {
+      if (googleCurrentUser == null) googleCurrentUser = await _googleSignIn.signInSilently();
+      if (googleCurrentUser == null) googleCurrentUser = await _googleSignIn.signIn();
+      if (googleCurrentUser == null) return null;
+
+      GoogleSignInAuthentication googleAuth = await googleCurrentUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+      print("signed in " + user.displayName);
+
+      return user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    _handleSignIn();
+
     return MaterialApp(
       title: 'Bacchus',
       theme: ThemeData(
