a200e1585ecfacf6a28c2b8c4cca789b01221708
sesta <sesta.ec5@gmail.com>
2020-06-27 13:54:47 +0900

    正方形が並ぶようにする

--- flutter/lib/app/widget/sake_grid.dart
+++ flutter/lib/app/widget/sake_grid.dart
@@ -10,18 +10,25 @@ class SakeGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     print(sakes);
     return GridView.count(
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 10,
       crossAxisCount: 2,
+      crossAxisSpacing: 8,
+      mainAxisSpacing: 8,
+      padding: EdgeInsets.all(8),
+      childAspectRatio: 1,
       children: sakes.map<Widget>((sake) {
         return Hero(
           tag: sake.thumbImageUrl,
-          child: FlatButton(
-            child: Image(
-              image: NetworkImage(sake.thumbImageUrl),
+          child: GestureDetector(
+            child: Material(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+              clipBehavior: Clip.antiAlias,
+              child: Image(
+                image: NetworkImage(sake.thumbImageUrl),
+                fit: BoxFit.cover,
+              ),
             ),
-            onPressed: () => Navigator.of(context).pushNamed('/sake', arguments: sake.thumbImageUrl),
-          )
+            onTap: () => Navigator.of(context).pushNamed('/sake', arguments: sake.thumbImageUrl),
+          ),
         );
       }).toList(),
     );
