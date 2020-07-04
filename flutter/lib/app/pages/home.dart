1d4fd8375bc9e55c52db1ba268b915be67f23fb8
sesta <sesta.ec5@gmail.com>
2020-07-04 14:59:24 +0900

    ホームのデザインを整える

--- flutter/lib/app/pages/home.dart
+++ flutter/lib/app/pages/home.dart
@@ -41,29 +41,86 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Bacchus'),
-        centerTitle: false,
-        elevation: 0,
-      ),
-      body: drinks.length == 0 ?
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(40),
-              child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              top: MediaQuery.of(context).padding.top + 8,
+              left: 16,
+              right: 16,
+            ),
+            child: Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Bacchus',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            )
-          ],
-        ) :
-        DrinkGrid(drinks: drinks),
+            ),
+          ),
+          drinks.length == 0 ?
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(40),
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
+                  ),
+                )
+              ],
+            ) :
+            DrinkGrid(drinks: drinks),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Theme.of(context).primaryColor,
+        shape: CircularNotchedRectangle(),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16.0, left: 16, right: 16),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Icon(
+                  Icons.home,
+                  size: 32,
+                  color: Colors.white,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Icon(
+                  Icons.people,
+                  size: 32,
+                  color: Theme.of(context).primaryColorLight,
+                ),
+              ),
+              Container(
+                width: 88,
+                height: 40,
+              ),
+              Expanded(
+                flex: 2,
+                child: Icon( // 場所の調整のために見えない要素を置く
+                  Icons.no_sim,
+                  size: 32,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _movePostPage,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
