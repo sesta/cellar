7ca8c47e3810b72045a66767da0bd8cb0564ee1a
sesta <sesta.ec5@gmail.com>
2020-07-10 09:52:24 +0900

    スクロールでpopできるようにする

--- flutter/lib/app/pages/drink.dart
+++ flutter/lib/app/pages/drink.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
@@ -46,183 +47,190 @@ class _DrinkPageState extends State<DrinkPage> {
     });
   }
 
+  Future<void> _popPage() async {
+    Navigator.of(context).pop(true);
+  }
+
   @override
   Widget build(BuildContext context) {
     final imageRatio = widget.drink.firstImageWidth/widget.drink.firstImageHeight;
     final imageLength = widget.drink.imagePaths.length;
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Stack(
+      body: RefreshIndicator(
+        onRefresh: _popPage,
+        color: Theme.of(context).scaffoldBackgroundColor,
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height,
+            ),
+            child: Column(
               children: <Widget>[
-                Container(
-                  color: Theme.of(context).primaryColor,
-                  child: CarouselSlider(
-                    options: CarouselOptions(
-                      aspectRatio: imageRatio,
-                      viewportFraction: 1,
-                      enableInfiniteScroll: false,
-                      onPageChanged: _updatePage,
-                    ),
-                    items: List.generate(imageLength, (index) {
-                      Widget content = Center(
-                        child: CircularProgressIndicator(
-                          strokeWidth: 2,
-                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                        ),
-                      );
-
-                      if (imageLoaded) {
-                        content = Image(
-                          image: NetworkImage(widget.drink.imageUrls[index]),
-                          fit: BoxFit.contain,
-                        );
-                      }
-
-                      if (index == 0) {
-                        content = Hero(
-                          tag: widget.drink.thumbImageUrl,
-                          child: imageLoaded
-                            ? content
-                            : Image(
-                              image: NetworkImage(widget.drink.thumbImageUrl),
-                              fit: BoxFit.contain,
-                            ),
-                        );
-                      }
-
-                      return GestureDetector(
-                        onVerticalDragEnd: (event) {
-                          if (event.velocity.pixelsPerSecond.dy > 100) {
-                            Navigator.of(context).pop(true);
-                          }
-                        },
-                        child: AspectRatio(
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      color: Theme.of(context).primaryColor,
+                      child: CarouselSlider(
+                        options: CarouselOptions(
                           aspectRatio: imageRatio,
-                          child: content,
+                          viewportFraction: 1,
+                          enableInfiniteScroll: false,
+                          onPageChanged: _updatePage,
                         ),
-                      );
-                    }).toList(),
+                        items: List.generate(imageLength, (index) {
+                          Widget content = Center(
+                            child: CircularProgressIndicator(
+                              strokeWidth: 2,
+                              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                            ),
+                          );
+
+                          if (imageLoaded) {
+                            content = Image(
+                              image: NetworkImage(widget.drink.imageUrls[index]),
+                              fit: BoxFit.contain,
+                            );
+                          }
+
+                          if (index == 0) {
+                            content = Hero(
+                              tag: widget.drink.thumbImageUrl,
+                              child: imageLoaded
+                                ? content
+                                : Image(
+                                  image: NetworkImage(widget.drink.thumbImageUrl),
+                                  fit: BoxFit.contain,
+                                ),
+                            );
+                          }
+
+                          return AspectRatio(
+                            aspectRatio: imageRatio,
+                            child: content,
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).padding.top,
+                        left: 16,
+                      ),
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.black87.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.close,
+                            size: 24,
+                            color: Colors.white
+                          ),
+                          padding: EdgeInsets.all(8),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Padding(
+                        padding: EdgeInsets.all(16),
+                        child: NormalText("${carouselPage + 1} / $imageLength"),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 16,
+                    left: 16,
+                    right: 16,
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(right: 4.0),
+                        child: Icon(
+                          Icons.person,
+                          color: Theme.of(context).accentColor,
+                        ),
+                      ),
+                      Expanded(
+                        child: NormalText(widget.drink.userName),
+                      ),
+                      NormalText(widget.drink.postDatetimeString),
+                    ],
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).padding.top,
+                    top: 24,
                     left: 16,
+                    right: 16,
                   ),
-                  child: Container(
-                    height: 40,
-                    width: 40,
-                    decoration: BoxDecoration(
-                      color: Colors.black87.withOpacity(0.3),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.close,
-                        size: 24,
-                        color: Colors.white
-                      ),
-                      padding: EdgeInsets.all(8),
-                      onPressed: () => Navigator.pop(context),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: Padding(
-                    padding: EdgeInsets.all(16),
-                    child: NormalText("${carouselPage + 1} / $imageLength"),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 16,
-                left: 16,
-                right: 16,
-              ),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(right: 4.0),
-                    child: Icon(
-                      Icons.person,
-                      color: Theme.of(context).accentColor,
-                    ),
-                  ),
-                  Expanded(
-                    child: NormalText(widget.drink.userName),
-                  ),
-                  NormalText(widget.drink.postDatetimeString),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 24,
-                left: 16,
-                right: 16,
-              ),
-              child: MainText(
-                widget.drink.drinkName,
-                bold: true,
-                multiLine: true,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 8,
-                left: 16,
-                right: 16,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(5, (i)=> i).map<Widget>((index) =>
-                  Padding(
-                    padding: EdgeInsets.only(left: 4, right: 4),
-                    child: Icon(
-                      index < widget.drink.score ? Icons.star : Icons.star_border,
-                      color: Colors.orangeAccent,
-                    ),
-                  )
-                ).toList(),
-              ),
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              padding: const EdgeInsets.only(
-                top: 32,
-                left: 16,
-                right: 16,
-                bottom: 64,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 8),
-                    child: Wrap(
-                      children: <Widget>[
-                        LabelText(widget.drink.drinkTypeLabel),
-                        widget.drink.price == 0 ? Container() : LabelText(widget.drink.priceString),
-                        widget.drink.place == '' ? Container() : LabelText(widget.drink.place),
-                      ],
-                    ),
-                  ),
-                  NormalText(
-                    widget.drink.memo,
+                  child: MainText(
+                    widget.drink.drinkName,
+                    bold: true,
                     multiLine: true,
+                    textAlign: TextAlign.center,
                   ),
-                ],
-              )
-            )
-          ]
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 8,
+                    left: 16,
+                    right: 16,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(5, (i)=> i).map<Widget>((index) =>
+                      Padding(
+                        padding: EdgeInsets.only(left: 4, right: 4),
+                        child: Icon(
+                          index < widget.drink.score ? Icons.star : Icons.star_border,
+                          color: Colors.orangeAccent,
+                        ),
+                      )
+                    ).toList(),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: const EdgeInsets.only(
+                    top: 32,
+                    left: 16,
+                    right: 16,
+                    bottom: 64,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 8),
+                        child: Wrap(
+                          children: <Widget>[
+                            LabelText(widget.drink.drinkTypeLabel),
+                            widget.drink.price == 0 ? Container() : LabelText(widget.drink.priceString),
+                            widget.drink.place == '' ? Container() : LabelText(widget.drink.place),
+                          ],
+                        ),
+                      ),
+                      NormalText(
+                        widget.drink.memo,
+                        multiLine: true,
+                      ),
+                    ],
+                  )
+                )
+              ]
+            ),
+          ),
         ),
       )
     );
