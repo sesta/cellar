d0d570912d3f657075760853cb6f223301a87e75
sesta <sesta.ec5@gmail.com>
2020-07-23 18:12:20 +0900

    SettingPageも調整

--- flutter/lib/app/pages/setting.dart
+++ flutter/lib/app/pages/setting.dart
@@ -15,14 +15,14 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingState extends State<SettingPage> {
-  bool loading = false;
-  final nameController = TextEditingController();
+  bool _loading = false;
+  final _nameController = TextEditingController();
 
   @override
   initState() {
     super.initState();
 
-    nameController.text = widget.user.userName;
+    _nameController.text = widget.user.userName;
   }
 
   _saveUser() async {
@@ -31,69 +31,69 @@ class _SettingState extends State<SettingPage> {
     }
 
     setState(() {
-      this.loading = true;
+      _loading = true;
     });
 
-    widget.user.userName = nameController.text;
+    widget.user.userName = _nameController.text;
     await widget.user.updateName();
 
     Navigator.of(context).pop();
   }
 
   get disableSave {
-    return nameController.text == '';
+    return _nameController.text == '';
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('設定'),
-        ),
-        body: Stack(
-          children: <Widget>[
-            Padding(
-            padding: EdgeInsets.all(64),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  NormalText('ニックネーム'),
-                  NormalTextField(
-                    nameController,
-                    onChanged: (_) => setState(() {}),
-                    bold: true,
-                  ),
-                  Padding(padding: EdgeInsets.only(bottom: 48)),
+      appBar: AppBar(
+        title: Text('設定'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Padding(
+          padding: EdgeInsets.all(64),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                NormalText('ニックネーム'),
+                NormalTextField(
+                  _nameController,
+                  onChanged: (_) => setState(() {}),
+                  bold: true,
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 48)),
 
-                  Center(
-                    child: RaisedButton(
-                      onPressed: disableSave ? null : _saveUser,
-                      child: Text(
-                        '更新',
-                        style: TextStyle(
-                          fontSize: 14,
-                        ),
-                      ),
-                      color: Theme.of(context).accentColor,
-                      textColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4),
+                Center(
+                  child: RaisedButton(
+                    onPressed: disableSave ? null : _saveUser,
+                    child: Text(
+                      '更新',
+                      style: TextStyle(
+                        fontSize: 14,
                       ),
                     ),
+                    color: Theme.of(context).accentColor,
+                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            loading ? Container(
-              color: Colors.black38,
-              alignment: Alignment.center,
-              child: CircularProgressIndicator(
-                strokeWidth: 2,
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-              ),
-            ) : Container(),
-          ],
-        )
+          ),
+          _loading ? Container(
+            color: Colors.black38,
+            alignment: Alignment.center,
+            child: CircularProgressIndicator(
+              strokeWidth: 2,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          ) : Container(),
+        ],
+      )
     );
   }
 }
