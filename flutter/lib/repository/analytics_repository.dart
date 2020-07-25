2222f9a3e9bb1aeb824823073da832f93effa9c1
sesta <sesta.ec5@gmail.com>
2020-07-25 21:06:09 +0900

    送るのに失敗してたくさい

--- flutter/lib/repository/analytics_repository.dart
+++ flutter/lib/repository/analytics_repository.dart
@@ -12,6 +12,25 @@ enum EventType {
   EditUserName,
 }
 
+extension EventTypeExtension on EventType {
+  // Firebase Analyticsのイベントはアルファベット + '_' しか許してくれないので整形
+  // https://firebase.google.com/docs/reference/cpp/group/event-names
+  String get name {
+    switch(this) {
+      case EventType.PostDrink: return 'post_drink';
+      case EventType.EditDrink: return 'edit_drink';
+      case EventType.DeleteDrink: return 'delete_drink';
+      case EventType.ChangeTimelineType: return 'change_timeline_type';
+      case EventType.ChangeDrinkType: return 'change_drink_type';
+      case EventType.ChangeOrderType: return 'change_order_type';
+      case EventType.ReloadTimeline: return 'reload_timeline';
+      case EventType.EditUserName: return 'edit_user_name';
+    }
+
+    throw '存在しないtypeです。 $this';
+  }
+}
+
 class AnalyticsRepository {
   static final FirebaseAnalytics _analytics = FirebaseAnalytics();
   static final FirebaseAnalyticsObserver _observer = FirebaseAnalyticsObserver(
@@ -27,7 +46,7 @@ class AnalyticsRepository {
     Map<String, String> parameters,
   ) async{
     await _analytics.logEvent(
-      name: eventType.toString(),
+      name: eventType.name,
       parameters: parameters,
     );
   }
