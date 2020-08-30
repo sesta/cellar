73bc243b5858e22a6bf7bd622c6a79c32959b4d0
sesta <sesta.ec5@gmail.com>
2020-08-30 20:44:36 +0900

    円グラフを表示できるようにする

--- flutter/lib/app/widget/summary.dart
+++ flutter/lib/app/widget/summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 import 'package:cellar/domain/entity/entities.dart';
 import 'package:cellar/repository/drink_repository.dart';
@@ -26,15 +27,52 @@ class _SummaryState extends State<Summary> {
       setState(() {
         this.drinks = drinks;
       });
-      print(drinks);
     });
   }
 
+  List<charts.Series<_ChartData, int>> get _seriesList {
+    final List<_ChartData> data = widget.user.uploadCounts.entries
+      .map((entry) => _ChartData(entry.key, entry.value))
+      .where((chartData) => chartData.uploadCount > 0)
+      .toList();
+
+    return [
+      charts.Series<_ChartData, int>(
+        id: 'Drinks',
+        domainFn: (_ChartData chartData, _) => chartData.drinkType.index,
+        measureFn: (_ChartData chartData, _) => chartData.uploadCount,
+        data: data,
+        labelAccessorFn: (_ChartData chartData, _) => chartData.drinkType.label,
+      )
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(widget.user.userId)
+      child: charts.PieChart(
+        _seriesList,
+        animate: true,
+        defaultRenderer: charts.ArcRendererConfig(
+          arcRendererDecorators: [
+            charts.ArcLabelDecorator(
+              labelPosition: charts.ArcLabelPosition.inside,
+            )
+          ]
+        ),
+      )
     );
+  }
+}
+
+class _ChartData {
+  final DrinkType drinkType;
+  final int uploadCount;
+
+  _ChartData(this.drinkType, this.uploadCount);
+
+  @override
+  String toString() {
+    return '$drinkType : $uploadCount';
   }
 }
