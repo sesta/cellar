56637e226071ca8af18115a913e815deefb348f6
sesta <sesta.ec5@gmail.com>
2020-08-30 16:23:46 +0900

    Summaryを出すためのrepositoryと初期化を追加

--- flutter/lib/app/widget/summary.dart
+++ flutter/lib/app/widget/summary.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:cellar/domain/entity/entities.dart';
+import 'package:cellar/repository/drink_repository.dart';
 
 class Summary extends StatefulWidget {
   Summary({
@@ -15,6 +16,20 @@ class Summary extends StatefulWidget {
 }
 
 class _SummaryState extends State<Summary> {
+  List<Drink> drinks = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    DrinkRepository().getUserAllDrinks(widget.user.userId).then((drinks) {
+      setState(() {
+        this.drinks = drinks;
+      });
+      print(drinks);
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
