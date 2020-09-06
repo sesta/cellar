1b2f5bad67601430997acf5971adf6c6ae9fd4a0
sesta <sesta.ec5@gmail.com>
2020-09-06 12:54:47 +0900

    表示する内容を修正

--- firebase/functions/src/triggers/postDrinkTrigger.ts
+++ firebase/functions/src/triggers/postDrinkTrigger.ts
@@ -6,7 +6,12 @@ export const postDrinkTrigger = (collectionName: String) =>
     .document(`${collectionName}/{drinkId}`)
     .onCreate((snapshot) => {
       const drink = snapshot.data()
-      const detail = `お酒の名前: ${drink.name}`
+      const detail = `
+userName: ${drink.userName}
+drinkName: ${drink.drinkName}
+drinkType: ${drink.drinkType}
+memo: ${drink.memo}
+      `
       const dev = collectionName.indexOf('dev') === -1 ? '' : '【開発】'
 
       void sendSlack(`${dev}お酒が投稿されました。`, detail)
