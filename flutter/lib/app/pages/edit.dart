98269eee54a5507a303f58c89f0bbcc1d224e278
sesta <sesta.ec5@gmail.com>
2020-12-25 21:41:02 +0900

    非公開の時は減らしてはいけない

--- flutter/lib/app/pages/edit.dart
+++ flutter/lib/app/pages/edit.dart
@@ -191,7 +191,9 @@ class _EditPageState extends State<EditPage> {
 
     await widget.drink.delete();
     await widget.user.decrementUploadCount(widget.drink.drinkType);
-    await widget.status.decrementUploadCount(widget.drink.drinkType);
+    if (!widget.drink.isPrivate) {
+      await widget.status.decrementUploadCount(widget.drink.drinkType);
+    }
 
     AnalyticsRepository().sendEvent(
       EventType.DeleteDrink,
