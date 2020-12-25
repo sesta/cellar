15f6e9b1868642f20c25d4dab87723ad94c01867
sesta <sesta.ec5@gmail.com>
2020-12-25 21:37:14 +0900

    修正前のdrinkTypeで減らす方が正しい

--- flutter/lib/app/pages/edit.dart
+++ flutter/lib/app/pages/edit.dart
@@ -190,8 +190,8 @@ class _EditPageState extends State<EditPage> {
     });
 
     await widget.drink.delete();
-    await widget.user.decrementUploadCount(_drinkType);
-    await widget.status.decrementUploadCount(_drinkType);
+    await widget.user.decrementUploadCount(widget.drink.drinkType);
+    await widget.status.decrementUploadCount(widget.drink.drinkType);
 
     AnalyticsRepository().sendEvent(
       EventType.DeleteDrink,
